5be51beb4b63e59722e5a56892c71b298e85b8ce
WeiChien Hsu <weichien711@gmail.com>
2018-03-31 00:19:35 -0500

    :construction: Connect PostgreSQL & Server by Feather

--- my-app/src/models/message.model.js
+++ my-app/src/models/message.model.js
@@ -6,9 +6,11 @@ const DataTypes = Sequelize.DataTypes;
 module.exports = function (app) {
   const sequelizeClient = app.get('sequelizeClient');
   const message = sequelizeClient.define('message', {
-    text: {
-      type: DataTypes.STRING,
-      allowNull: false
+    name: {
+      type: DataTypes.CHAR,
+    },
+    message: {
+      type: DataTypes.TEXT,
     }
   }, {
     hooks: {
